46401a5d39fd3da4ec365368c590b603727e3ec3
Sergey Averin <averin_sd@icloud.com>
2024-05-27 18:04:22 +0300

    Create page with information about website

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/more', function (req, res, next) {
+  res.render('more')
+})
+
 module.exports = router;
